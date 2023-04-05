507f3f69bd88ce43c4fafd82643e74ebcf211693
shivscaler <129844674+shivscaler@users.noreply.github.com>
2023-04-05 06:34:00 +0000

    update test

--- Lecture1_Asgn1_Editorial_Solution/src/test/java/CircleTest.java
+++ Lecture1_Asgn1_Editorial_Solution/src/test/java/CircleTest.java
@@ -8,6 +8,16 @@ import java.lang.reflect.Method;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class CircleTest {
+    private Circle circle;
+    private ByteArrayOutputStream outContent;
+
+    private Point point;
+
+    @BeforeEach
+    void setUp() {
+        circle = new Circle();
+        point = new Point();
+    }
 
     @Test
     public void testGetArea() {
@@ -23,10 +33,15 @@ public class CircleTest {
         assertEquals(Math.PI * 8, c.getParameter(), 0.001);
     }
 
+
     @Test
     public void testRadiusExists() {
-        Circle c = new Circle();
-        assertNotNull(c.radius);
+        try {
+            Field radiusField = circle.getClass().getDeclaredField("radius");
+            assertEquals(radiusField.getType().toString(), "int");
+        } catch (NoSuchFieldException e) {
+            fail("radius data member of type int not found");
+        }
     }
 
 }
